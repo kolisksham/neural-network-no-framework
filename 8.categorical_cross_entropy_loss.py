aa66dfcfe1d426211da85df4b8eee33c28b70b2b
kolisksham <kolisksham@gmail.com>
2026-07-16 19:05:51 +0530

    example with one hot encoded class target.

--- 8.categorical_cross_entropy_loss.py
+++ 8.categorical_cross_entropy_loss.py
@@ -3,7 +3,7 @@ import numpy as np
 # CASE I: Labels / Targets are Numbers
 softmax_outputs = np.array([
     [0.7, 0.1, 0.2],
-    [0.1, 0.9, 0.4],
+    [0.1, 0.5, 0.4],
     [0.02, 0.9, 0.08]
 ])
 
@@ -21,3 +21,25 @@ print(avg_loss)
 print("="*100)
 
 # CASE II: Labels / Targets are One Hot Encoded
+
+enc_class_targets = np.array([
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 1, 0]
+])
+
+loss_matrix = softmax_outputs * enc_class_targets
+loss_vector = np.sum(loss_matrix, axis=1)
+
+print(f"Loss Vector: {loss_vector}")
+print("="*100)
+
+enc_neg_log = -np.log(loss_vector)
+print(f"Negative Log: {enc_neg_log}")
+print("="*100)
+
+enc_avg_loss = np.mean(enc_neg_log)
+print(f"Average Loss: {enc_avg_loss}")
+print("="*100)
+
+# main implementation
