c4b32699f8b31e9abe3ed9b9bb0a9a9a3a6286d6
kolisksham <kolisksham@gmail.com>
2026-07-24 19:35:23 +0530

    Categorial Loss Function implimentation.

--- 8.categorical_cross_entropy_loss.py
+++ 8.categorical_cross_entropy_loss.py
@@ -7,7 +7,7 @@ softmax_outputs = np.array([
     [0.02, 0.9, 0.08]
 ])
 
-class_targets = [0, 1, 1]
+class_targets = np.array([0, 1, 1])
 print(softmax_outputs[[0, 1, 2], class_targets])
 print("="*100)
 
@@ -43,3 +43,30 @@ print(f"Average Loss: {enc_avg_loss}")
 print("="*100)
 
 # main implementation
+
+class Loss:
+    def calculate(self, outputs, y):
+        sample_losses = self.forward(outputs, y)
+        mean_losses = np.mean(sample_losses)
+        return mean_losses
+
+class Loss_CategorialCrossEntropy(Loss):
+    def forward(self, y_pred, y_test):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+
+        if len(y_test.shape) == 1:
+            confidence = -np.log(y_pred_clipped[range(samples), y_test])
+        elif len(y_test.shape) == 2:
+            confidence = -np.log(np.sum(y_pred_clipped*y_test, axis=1))
+        return confidence
+
+calculate_loss = Loss_CategorialCrossEntropy()
+mean_loss = calculate_loss.calculate(softmax_outputs, class_targets)
+mean_loss_enc = calculate_loss.calculate(softmax_outputs, enc_class_targets)
+
+print("="*100)
+print(f"1-D Class Targets Losses:\n{mean_loss}")
+print("="*100)
+print(f"2-D Class Targets Losses:\n{mean_loss_enc}")
+print("="*100)
